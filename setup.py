d53ce2eb47f6f21842931af7b3eabf51ed867daf
Thomas Capelle <tcapelle@pm.me>
2023-04-05 11:11:56 +0200

    setup and Pypi release

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+exec(open("miniminiai/version.py").read())
+
+setup(
+    name="miniminiai",
+    packages=find_packages(),
+    version=__version__,
+    license="MIT",
+    description="A mini version of fastai's miniai",
+    author="Johno Whitaker",
+    url="https://github.com/johnowhitaker/miniminiai",
+    long_description_content_type="text/markdown",
+    keywords=[
+        "artificial intelligence",
+        "pytorch",
+        "machine learning",
+        "machine learning framework",
+        "fastai",
+    ],
+    install_requires=[
+        "torch",
+        "fastcore",
+        "numpy",
+        "matplotlib",
+        "torcheval",
+
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
